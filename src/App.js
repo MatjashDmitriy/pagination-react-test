bd9ff8c5087b74d3074e5c4576cfbe2be6203f86
MatjashDmitriy <gorbachov1991@gmail.com>
2023-04-27 12:47:31 +0300

    create new file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>ВЫ АВТОРИЗИРОВАНЫ (❛‿❛✿̶̥̥)</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
